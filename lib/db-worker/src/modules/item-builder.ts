4e889d32b3c6837cc924bfcd201953edced8af56
omargharb <151344648+omargharb@users.noreply.github.com>
2023-11-29 14:58:12 +0200

    Update item-builder.ts
    allows date field to take full value, removes ".split("-")[0];

--- lib/db-worker/src/modules/item-builder.ts
+++ lib/db-worker/src/modules/item-builder.ts
@@ -55,7 +55,7 @@ export default class ItemBuilder {
       const fields = itemFields[itemID].reduce((fields, field) => {
         let { value } = field;
         if (field.fieldName === "date") {
-          value = multipartToSQL(value as string).split("-")[0];
+          value = multipartToSQL(value as string); // Remove .split("-")[0];
         }
         (fields[field.fieldName] ??= []).push(value);
         return fields;
